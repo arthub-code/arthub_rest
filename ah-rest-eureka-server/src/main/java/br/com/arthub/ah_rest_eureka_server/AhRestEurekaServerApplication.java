df2b19edffc82a8a839033a8a5658be24c2f01e6
mistervb <dragonxdgames@gmail.com>
2024-09-22 15:20:33 -0300

    arthub: gateway service boilerplate & eureka server boilerplate

---
+++ ah-rest-eureka-server/src/main/java/br/com/arthub/ah_rest_eureka_server/AhRestEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthub.ah_rest_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhRestEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhRestEurekaServerApplication.class, args);
+	}
+
+}
