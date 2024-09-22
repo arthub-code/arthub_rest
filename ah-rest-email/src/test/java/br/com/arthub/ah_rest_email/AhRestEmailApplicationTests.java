24f38e6c29578f3c4c86d846e93b4366de50ff54
mistervb <dragonxdgames@gmail.com>
2024-09-22 15:08:53 -0300

    arthub: email service boilerplate

---
+++ ah-rest-email/src/test/java/br/com/arthub/ah_rest_email/AhRestEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthub.ah_rest_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhRestEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
