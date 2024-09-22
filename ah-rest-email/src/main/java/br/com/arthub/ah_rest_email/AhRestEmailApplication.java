16a639be695fe40d75892bd863a84d2c58336f8e
Mr.VB <dragonxdgames@gmail.com>
2024-09-22 15:21:51 -0300

    arthub: email service boilerplate

---
+++ ah-rest-email/src/main/java/br/com/arthub/ah_rest_email/AhRestEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthub.ah_rest_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhRestEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhRestEmailApplication.class, args);
+	}
+
+}
