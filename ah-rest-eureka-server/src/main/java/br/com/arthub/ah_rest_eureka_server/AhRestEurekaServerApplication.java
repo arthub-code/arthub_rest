8aa0605e95d142b0a294171d7fe4033a257bfab0
mistervb <dragonxdgames@gmail.com>
2024-09-23 16:18:53 -0300

    arthub: (UPDATE IN application.yml) add enable eureka server anotation and fix application names

--- ah-rest-eureka-server/src/main/java/br/com/arthub/ah_rest_eureka_server/AhRestEurekaServerApplication.java
+++ ah-rest-eureka-server/src/main/java/br/com/arthub/ah_rest_eureka_server/AhRestEurekaServerApplication.java
@@ -2,8 +2,10 @@ package br.com.arthub.ah_rest_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AhRestEurekaServerApplication {
 
 	public static void main(String[] args) {
