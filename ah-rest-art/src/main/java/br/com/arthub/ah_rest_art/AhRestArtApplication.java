e96a338fe2e664f64b496bd0dca69dcc259568a6
Mr.VB <dragonxdgames@gmail.com>
2024-10-03 10:00:32 -0300

    [ADD] Novo microserviço: Arte
    - Ajuste no gateway config adicionando a rota do o novo microserviço;
    - Ajuste no eureka server registrando o novo microserviço;
    - Ajuste no docker-compose adicionando o novo serviço no arquivo;
    - Add dockerfile para o novo microserviço
    - Add nova aplicação spring com boilerplate inicial;

---
+++ ah-rest-art/src/main/java/br/com/arthub/ah_rest_art/AhRestArtApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthub.ah_rest_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhRestArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhRestArtApplication.class, args);
+	}
+
+}
